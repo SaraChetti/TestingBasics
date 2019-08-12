08c4c3ad7d834d2f3dce0bc47ae5406d848a8a69
saravananit43@gmail.com <saravananit43@gmail.com>
2019-08-12 13:04:54 +0530

    New project added

---
+++ ForDemo/src/com/test/first/PractiseOne.java
@@ -0,0 +1,19 @@
+package com.test.first;
+
+public class PractiseOne {
+	public static void main(String[] args) {
+		
+		login("sara","sara");
+		
+	}
+
+
+	
+	public static void login(String empId, String empPass){
+		
+		System.out.println("i am login in to application"+empId);
+		
+	}
+	 }
+	
+
